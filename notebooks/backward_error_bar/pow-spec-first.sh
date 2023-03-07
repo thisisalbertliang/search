b891086121a7166eafcdeec96f8f3c3435ccfbd5
Albert Liang <albertliang00@gmail.com>
2023-03-07 17:48:50 -0500

    Implement error bar plots for backward model power spectrum

---
+++ notebooks/backward_error_bar/pow-spec-first.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python violin.py --dropout-prob 0.1
+
+python violin.py --dropout-prob 0.2
+
+python violin.py --dropout-prob 0.3
+
+python violin.py --dropout-prob 0.4
+
+python violin.py --dropout-prob 0.5
+
+python violin.py --dropout-prob 0.6
+
+python violin.py --dropout-prob 0.7
+
+python violin.py --dropout-prob 0.8
+
+python violin.py --dropout-prob 0.9
---
+++ notebooks/backward_error_bar/pow-spec-last.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python violin.py --dropout-prob 0.1 --pow-spec-last
+
+python violin.py --dropout-prob 0.2 --pow-spec-last
+
+python violin.py --dropout-prob 0.3 --pow-spec-last
+
+python violin.py --dropout-prob 0.4 --pow-spec-last
+
+python violin.py --dropout-prob 0.5 --pow-spec-last
+
+python violin.py --dropout-prob 0.6 --pow-spec-last
+
+python violin.py --dropout-prob 0.7 --pow-spec-last
+
+python violin.py --dropout-prob 0.8 --pow-spec-last
+
+python violin.py --dropout-prob 0.9 --pow-spec-last
